2d8d610d84717209f7a772504a040ffba7008db4
James Guo <tigerguoxin@gmail.com>
2015-11-19 17:08:30 +0000

    20151119 solve level 2

--- level2.py
+++ level2.py
@@ -1,11 +1,16 @@
 import urllib2
+import re
 
 url = "http://www.pythonchallenge.com/pc/def/ocr.html"
-
+key = "#@!$%+{}[]_-&*()*^@/"
 def main():
     page = urllib2.urlopen(url)
     data = page.read()
-    print data
+    result = ""
+    for letter in data:
+        if letter not in key:
+            result += letter
+    print result
 
 if __name__ == "__main__":
     main()
